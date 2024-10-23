041c5d7a6f20793958fed84286e5c05d6bf8e0d3
Chethan-chethu <chethanr162000@gmail.com>
2024-10-23 23:11:11 +0530

    first commit on initialize

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function () {
+  $("#news-slider").owlCarousel({
+    items: 3,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [980, 2],
+    itemsMobile: [600, 1],
+    navigation: true,
+    navigationText: ["", ""],
+    pagination: true,
+    autoPlay: true
+  });
+   
+  $('.post-img>img').on('click', function() {
+      window.open($(this).attr('src'), '_blank');        
+  });
+
+  $('.post-img>img').hover( function() {      
+    $(this).addClass('img-hover');       
+  },
+  function(){
+      $(this).removeClass('img-hover');
+    });  
+    
+});
